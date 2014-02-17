047b79cca3e58b98b40edb0fe076d501a6a9f859
PaulGreco <paul_greco@yahoo.com>
2014-02-17 12:25:13 -0600

    Test Commit
    This is a test

---
+++ unity/Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				if (Input.anyKeyDown)
+				{	
+						transform.position = new Vector3 (transform.position.x + 10 * Time.deltaTime, transform.position.y + 1, transform.position.z + 1);
+				}
+		}
+}
