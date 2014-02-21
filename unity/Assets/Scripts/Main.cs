48966a54a69d44e8e94fee81f9c72092a572f5f7
PaulGreco <paul_greco@yahoo.com>
2014-02-20 19:58:22 -0600

    Matrix class implemented
    There may be some errors, but a lot of it seems good. We'll find out
    tomorrow.

--- unity/Assets/Scripts/Main.cs
+++ unity/Assets/Scripts/Main.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Main : MonoBehaviour {
 
+	Matrix LowerArm;
+	Matrix MiddleArm;
+	Matrix UpperArm;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
--- unity/Assets/Scripts/Matrix.cs
+++ unity/Assets/Scripts/Matrix.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 
 public class Matrix
 {
-	private List<List<float>> m_matrix; 
+	public List<List<float>> m_matrix; 
 
+	#region Accessors
 	public int RowCount
 	{
 		get { return m_matrix.Count; }
@@ -15,14 +16,21 @@ public class Matrix
 	{
 		get { return m_matrix [0].Count; }
 	}
+	#endregion
 
+	#region Constructors
 	public Matrix(int rows, int columns)
 	{
-		m_matrix = new List<List<float>> (rows);
+		m_matrix = new List<List<float>>();
 
-		for(int row = 0; row < rows; rows++ 
+		for(int row = 0; row < rows; row++)
 		{
-			m_matrix[row] = new List<float>(columns);
+			m_matrix.Add(new List<float>());
+
+			for (int column = 0; column < columns; column++)
+			{
+				m_matrix[row].Add (0);
+			}
 		}
 	}
 
@@ -33,23 +41,106 @@ public class Matrix
 	/// <param name="columns">Columns.</param>
 	public Matrix( float[] array, int columns)
 	{
-		m_matrix = new List<List<float>> (array.Length / columns);
+		int rows = array.Length/columns;
+		m_matrix = new List<List<float>>();
 
-		foreach (List<float> row in m_matrix) {
-			row = new List<float>(columns);
+		int arrayLocation = 0;
+		for(int row = 0; row < rows; row++)
+		{
+			m_matrix.Add (new List<float>());
+			for (int setcolumn = 0; setcolumn < columns; setcolumn++)
+			{
+				m_matrix[row].Add(array[arrayLocation]);
+				arrayLocation++;
+			}
 		}
 	}
 
-	/// <summary>
-	/// Can be used for setting values in the matrix.	/// </summary>
-	/// <param name="row">Row.</param>
-	public List<float> GetRow(int row)
+	public Matrix(Matrix oldMatrix)
 	{
-		return m_matrix[row];
+		m_matrix = new List<List<float>>();
+
+		for (int row = 0; row < oldMatrix.RowCount; row++)
+		{
+			m_matrix.Add (new List<float>());
+
+			for (int column = 0; column < oldMatrix.ColumnCount; column++)
+			{
+				m_matrix[row].Add (oldMatrix[row][column]);
+			}
+		}
+	}
+	#endregion
+
+	/// <summary>
+	/// Assumes that the matrix object is already created a sized correctly.
+	/// Sets the values of a matrix. float.NaN values will be ignored	/// </summary>
+	/// <param name="array">Array.</param>
+	/// <param name="columns">Columns.</param>
+	public void SetMatrix( float[] array, int columns)
+	{
+		int rows = array.Length/columns;
+		
+		int arrayLocation = 0;
+		for(int row = 0; row < rows; row++)
+		{			
+			for (int setcolumn = 0; setcolumn < columns; setcolumn++)
+			{
+				if (!float.IsNaN(array[arrayLocation]))
+				{
+					m_matrix[row][setcolumn] = array[arrayLocation];
+				}
+				arrayLocation++;
+			}
+		}
+	}
+
+	public void SetMatrix( Matrix a)
+	{
+		for (int row = 0; row < a.RowCount; row++)
+		{
+			for(int column = 0; column < a.ColumnCount; column++)
+			{
+				m_matrix[row][column] = a[row][column];
+			}
+		}
+	}
+
+	private string m_printList(List<float> list)
+	{
+		string matrix = string.Empty;
+		foreach (float entry in list)
+		{
+			matrix += string.Format ("{0} ", entry);
+		}
+
+		return matrix;
+	}
+
+	public override string ToString ()
+	{
+		string matrixString = string.Empty;
+
+		foreach (List<float> row in m_matrix)
+		{
+			matrixString += "\n" + m_printList (row);
+		}
+
+		return matrixString;
 	}
 
 	/// <summary>
-	/// Should not be used for setting values in the matrix.	/// </summary>
+	/// Gets the specified row by value	/// </summary>
+	/// <returns>The row.</returns>
+	/// <param name="row">Row.</param>
+	public List<float> GetRow(int row)
+	{
+		return new List<float>(m_matrix[row]);
+	}
+
+	/// <summary>
+	/// Gets the specified column by value	/// </summary>
+	/// <returns>The column.</returns>
 	/// <param name="column">Column.</param>
 	public List<float> GetColumn(int column)
 	{
@@ -62,38 +153,130 @@ public class Matrix
 		return columnVector;
 	}
 
+	public void Print()
+	{
+		Debug.Log(this.ToString());
+	}
+
 	/// <summary>
-	/// Gets a value at the specified row and column	/// </summary>
-	/// <returns>The value.</returns>
+	/// Gets the specified row by reference.	/// </summary>
 	/// <param name="row">Row.</param>
-	/// <param name="column">Column.</param>
-	public float GetValue(int row, int column)
+	public List<float> this[int row]
 	{
-		return m_matrix [row] [column];
+			get{ return m_matrix[row]; }
 	}
 
-	public void SetValue(int row, int column, float value)
-	{
-		m_matrix [row] [column] = value;
-	}
-
+	#region Overloaded Operators
 	public static Matrix operator+(Matrix a, Matrix b)
 	{
-		Matrix result = null;
+		Matrix result = new Matrix (a.RowCount, a.ColumnCount);
 
-		if (a.RowCount != b.RowCount || a.ColumnCount != b.ColumnCount) {
-			result = new Matrix (a.RowCount, a.ColumnCount);
-
-			for (int row = 0; row < a.RowCount; row++)
+		for (int row = 0; row < a.RowCount; row++)
+		{
+			for (int column = 0; column < a.ColumnCount; column++)
 			{
-				for (int column = 0; column < a.ColumnCount; column++)
-				{
-					result.SetValue(row, column, a.GetValue(row,column)+ b.GetValue(row,column));
-				}
+				result[row][column] = a[row][column] + b[row][column];
 			}
 		}
+
 		return result;
 	}
+
+	public static Matrix operator+(float a, Matrix b)
+	{
+		Matrix result = new Matrix(b);
+
+		for (int row = 0; row < b.RowCount; row++)
+		{
+			for (int column = 0; column < b.ColumnCount; column++)
+			{
+				result[row][column] += a;
+			}
+		}
+		
+		return result;
+	}
+
+	public static Matrix operator+(Matrix b, float a)
+	{
+		return a+b;
+	}
+
+	public static Matrix operator-(Matrix a, Matrix b)
+	{
+		Matrix result = new Matrix (a.RowCount, a.ColumnCount);
+		
+		for (int row = 0; row < a.RowCount; row++)
+		{
+			for (int column = 0; column < a.ColumnCount; column++)
+			{
+				result[row][column] = a[row][column] - b[row][column];
+			}
+		}
+		
+		return result;
+	}
+
+	public static Matrix operator-(float a, Matrix b)
+	{
+		return (-1*a)+b;
+	}
+
+	public static Matrix operator-(Matrix b, float a)
+	{
+		return (-1*a)+b;
+	}
+
+	public static Matrix operator*(Matrix a, Matrix b)
+	{
+		Matrix result = new Matrix (a.RowCount, b.ColumnCount);
+		
+		for (int row = 0; row < a.RowCount; row++)
+		{
+			for (int column = 0; column < b.ColumnCount; column++)
+			{
+				float sum = 0;
+
+				for (int multiplier = 0; multiplier < a.RowCount; multiplier++)
+				{
+					sum += a[row][multiplier] * b[multiplier][column];
+				}
+				result[row][column] = sum;
+			}
+		}
+		
+		return result;
+	}
+
+	public static Matrix operator*(float a, Matrix b)
+	{
+		Matrix result = new Matrix (b);
+		
+		for (int row = 0; row < b.RowCount; row++)
+		{
+			for (int column = 0; column < b.ColumnCount; column++)
+			{
+				result[row][column] *= a;
+			}
+		}
+		
+		return result;
+	}
+
+	public static Matrix operator*(Matrix b, float a)
+	{
+		return a*b;
+	}
+
+	public static Matrix operator/(Matrix b, float a)
+	{
+		return (1/a)*b;
+	}
+	#endregion
+
+
 }
+
+
 
 
