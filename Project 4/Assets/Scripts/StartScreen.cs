5b2e0f793fbeae7d3c57b8bd69b4ce905142e665
PaulGreco <paul_greco@yahoo.com>
2014-04-06 16:36:46 -0500

    User Interface Complete
    WHOO!!!

--- Project 4/Assets/Scripts/StartScreen.cs
+++ Project 4/Assets/Scripts/StartScreen.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /*
 	Possible format for reading in Robot parameters from file:
@@ -19,8 +20,45 @@ using System.Collections;
 
 */
 
+public class Light
+{
+	public int m_id;
+	public float m_x, m_y, m_intensity;
+
+	public Light(int id, float x, float y, float intensity)
+	{
+		m_id = id;
+		m_x = x;
+		m_y = y;
+		m_intensity = intensity;
+	}
+}
+
+public class Robot
+{
+	public int m_id;
+	public float m_x, m_y, m_k11, m_k12, m_k21, m_k22;
+
+	public Robot(int id ,float x, float y, float k11, float k12, float k21, float k22)
+	{
+		m_id = id;
+		m_x = x;
+		m_y = y;
+		m_k11 = k11;
+		m_k12 = k12;
+		m_k21 = k21;
+		m_k22 = k22;
+	}
+}
+
 public static class SetupData
 {
+	public static List<Light> lightList = new List<Light>();
+	public static List<Robot> robotList = new List<Robot>();
+}
+
+public class StartScreen : MonoBehaviour {
+
 	public static string filename = string.Empty;
 	public static string xCoord = string.Empty;
 	public static string yCoord = string.Empty;
@@ -28,11 +66,11 @@ public static class SetupData
 	public static string k12 = string.Empty;
 	public static string k21 = string.Empty;
 	public static string k22 = string.Empty;
-}
+	public static string xlight = string.Empty;
+	public static string ylight = string.Empty;
+	public static string intensity = string.Empty;
 
-public class StartScreen : MonoBehaviour {
-
-	public enum selectedControl {setText, /*setNum,*/ x, y, k11, k12, k21, k22, none};
+	public enum selectedControl {setText, xbot, ybot, k11, k12, k21, k22, xlight, ylight, intensity, none};
 	selectedControl focus = selectedControl.none;
 //	public static string numBots = string.Empty;
 
@@ -42,169 +80,259 @@ public class StartScreen : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 
 		string input = getKeyboardInput ();
-		if (input != string.Empty) {
+		if (input != string.Empty) 
+		{
 			if (focus == selectedControl.setText) {
-				if (input == "b")
+				if (input == "\b")
 				{
-					if (SetupData.filename.Length > 0)
-						SetupData.filename = SetupData.filename.Remove (SetupData.filename.Length-1,1);
+					if (filename.Length > 0)
+					{
+						filename = filename.Remove(filename.Length-1,1);
+					}
 				}
 				else
 				{
-					SetupData.filename += input;
+					filename += input;
 				}
-			} /*else if (focus == selectedControl.setNum) {
-				if (input == "b")
+			} else if (focus == selectedControl.intensity) {
+				if (input == "\b")
 				{
-					if (numBots.Length > 0)
-						numBots = numBots.Remove (numBots.Length-1,1);
+					if (intensity.Length > 0)
+						intensity = intensity.Remove (intensity.Length-1,1);
 				}
 				else
 				{
+<<<<<<< HEAD
+					intensity += input;
+				}
+			} else if (focus == selectedControl.xbot) {
+				if (input == "\b")
+=======
 					numBots += input;
 				}
 			}*/ else if (focus == selectedControl.x) {
 				if (input == "b")
+>>>>>>> 39dc474133cff02730ec8aa687af693ac00f49cd
 				{
-					if (SetupData.xCoord.Length > 0)
-						SetupData.xCoord = SetupData.xCoord.Remove (SetupData.xCoord.Length-1,1);
+					if (xCoord.Length > 0)
+						xCoord = xCoord.Remove (xCoord.Length-1,1);
 				}
 				else
 				{
-					SetupData.xCoord += input;
+					xCoord += input;
 				}
-			} else if (focus == selectedControl.y) {
-				if (input == "b")
+			} else if (focus == selectedControl.ybot) {
+				if (input == "\b")
 				{
-					if (SetupData.yCoord.Length > 0)
-						SetupData.yCoord = SetupData.yCoord.Remove (SetupData.yCoord.Length-1,1);
+					if (yCoord.Length > 0)
+						yCoord = yCoord.Remove (yCoord.Length-1,1);
 				}
 				else
 				{
-					SetupData.yCoord += input;
+					yCoord += input;
 				}
 			} else if (focus == selectedControl.k11) {
-				if (input == "b")
+				if (input == "\b")
 				{
-					if (SetupData.k11.Length > 0)
-						SetupData.k11 = SetupData.k11.Remove (SetupData.k11.Length-1,1);
+					if (k11.Length > 0)
+						k11 = k11.Remove (k11.Length-1,1);
 				}
 				else
 				{
-					SetupData.k11 += input;
+					k11 += input;
 				}
 			} else if (focus == selectedControl.k12) {
-				if (input == "b")
+				if (input == "\b")
 				{
-					if (SetupData.k12.Length > 0)
-						SetupData.k12 = SetupData.k12.Remove (SetupData.k12.Length-1,1);
+					if (k12.Length > 0)
+						k12 = k12.Remove (k12.Length-1,1);
 				}
 				else
 				{
-					SetupData.k12 += input;
+					k12 += input;
 				}
 			} else if (focus == selectedControl.k21) {
-				if (input == "b")
+				if (input == "\b")
 				{
-					if (SetupData.k21.Length > 0)
-						SetupData.k21 = SetupData.k21.Remove (SetupData.k21.Length-1,1);
+					if (k21.Length > 0)
+						k21 = k21.Remove (k21.Length-1,1);
 				}
 				else
 				{
-					SetupData.k21 += input;
+					k21 += input;
 				}
 			} else if (focus == selectedControl.k22) {
-				if (input == "b")
+				if (input == "\b")
 				{
-					if (SetupData.k22.Length > 0)
-						SetupData.k22 = SetupData.k22.Remove (SetupData.k22.Length-1,1);
+					if (k22.Length > 0)
+						k22 = k22.Remove (k22.Length-1,1);
 				}
 				else
 				{
-					SetupData.k22 += input;
+					k22 += input;
+				}
+			}
+			else if (focus == selectedControl.xlight) {
+				if (input == "\b")
+				{
+					if (xlight.Length > 0)
+						xlight = xlight.Remove (xlight.Length-1,1);
+				}
+				else
+				{
+					xlight += input;
+				}
+			}
+			else if (focus == selectedControl.ylight) {
+				if (input == "\b")
+				{
+					if (ylight.Length > 0)
+						ylight = ylight.Remove (ylight.Length-1,1);
+				}
+				else
+				{
+					ylight += input;
 				}
 			}
 		}
 	}
 
+<<<<<<< HEAD
+	void parseFile()
+	{
+
+	}
+
+	string getKeyboardInput()
+=======
 	public string getKeyboardInput()
+>>>>>>> 39dc474133cff02730ec8aa687af693ac00f49cd
 	{
 		string ret = string.Empty;
-		if (Input.GetKeyDown (KeyCode.Alpha0)) {
-			ret = "0";
-		}
-		else if (Input.GetKeyDown (KeyCode.Alpha1)){
-			ret = "1";
-		}
-		else if (Input.GetKeyDown (KeyCode.Alpha2)) {
-			ret = "2";
-		}
-		else if (Input.GetKeyDown (KeyCode.Alpha3)){
-			ret = "3";
-		}
-		else if (Input.GetKeyDown (KeyCode.Alpha4)){
-			ret = "4";
-		}
-		else if (Input.GetKeyDown (KeyCode.Alpha5)) {
-			ret = "5";
-		}
-		else if (Input.GetKeyDown (KeyCode.Alpha6)){
-			ret = "6";
-		}
-		else if (Input.GetKeyDown (KeyCode.Alpha7)){
-			ret = "7";
-		}
-		else if (Input.GetKeyDown (KeyCode.Alpha8)){
-			ret = "8";
-		}
-		else if (Input.GetKeyDown (KeyCode.Alpha9)) {
-			ret = "9";
-		}
-		else if (Input.GetKeyDown (KeyCode.Period)){
-			ret = ".";
-		}
-		else if (Input.GetKeyDown (KeyCode.Backspace)){
-			ret = "b";
-		}
-		else
+		ret = Input.inputString;
+		if (ret.Length > 1)
 		{
-			ret = "";
+				ret = ret.Substring(0,1);
 		}
 		return ret;
 	}
-	
+
 	void OnGUI ()
 	{
-		GUI.TextArea (new Rect (0, 0, 160, 20), "Braitenberg Vehicles - Team Alpha");
-		GUI.TextArea (new Rect (396, 300, 80, 20), "Upload from file");		// Change to button later
-		GUI.TextArea (new Rect (346, 320, 100, 40), "Add robot");			// Change to button later
-		// "Start Simulation" Button needed still
+		GUI.TextArea (new Rect (0, 0, 250, 20), "Braitenberg Vehicles - Team Alpha");
+		GUI.TextArea (new Rect (326, 280, 70, 20), "File Name:");
+		GUI.TextArea (new Rect (326, 360, 70, 20), "Xcoord:");
+		GUI.TextArea (new Rect (326, 380, 70, 20), "Ycoord:");
+		GUI.TextArea (new Rect (326, 400, 70, 20), "K11:");
+		GUI.TextArea (new Rect (326, 420, 70, 20), "K12:");
+		GUI.TextArea (new Rect (326, 440, 70, 20), "K21:");
+		GUI.TextArea (new Rect (326, 460, 70, 20), "K22:");
 
+		GUI.TextArea (new Rect (636, 360, 60, 20), "xlight:");
+		GUI.TextArea (new Rect (636, 380, 60, 20), "ylight:");
+		GUI.TextArea (new Rect (636, 400, 60, 20), "intensity:");
 
-		// Need text boxes for all parameters
-			// "Add Robot" button will create Robot with given parameters and clear all parameters for new Robot, should user decide to add another
-
-		
-
-		// Reference vvvvvvvvvvvvvvvvv
-		/*	if (GUI.Button (new Rect (256,360,100,100), "START\n SERVER")) {
-			NetworkData.isServer = true;
-			Application.LoadLevel("PencilBot");
+		if (GUI.Button (new Rect(396, 300, 200, 20), "Upload data from file and start"))
+		{
+			parseFile ();
+			Application.LoadLevel ("Main");
 		}
-		if (GUI.Button (new Rect (456,360,100,100), "START\n CLIENT")) {
-			NetworkData.isServer = false;
-			Application.LoadLevel("PencilBot");
+
+		if (GUI.Button (new Rect(396,480,100,20), "Add Robot"))
+		{
+			float xf, yf, k11f, k12f, k21f, k22f;
+
+			if (float.TryParse (xCoord, out xf) &&
+			    float.TryParse (yCoord, out yf) &&
+			    float.TryParse (k11, out k11f) &&
+			    float.TryParse (k12, out k12f) &&
+			    float.TryParse (k21, out k21f) &&
+			    float.TryParse (k22, out k22f))
+			{
+				SetupData.robotList.Add (new Robot(SetupData.robotList.Count, xf,yf,k11f,k12f,k21f,k22f));
+			}
+
+			xCoord = string.Empty;
+			yCoord = string.Empty;
+			k11 = string.Empty;
+			k12 = string.Empty;
+			k21 = string.Empty;
+			k22 = string.Empty;
 		}
-		if (GUI.Button (new Rect (456,300,160,20), NetworkData.ipAddressString)) {
-			focus = selectedControl.ipAddress;
-			inputTimeout = 5;
+
+		if (GUI.Button (new Rect(396,280,200,20), filename))
+		{
+			focus = selectedControl.setText;
 		}
-		if (GUI.Button (new Rect (456,320,160,20), NetworkData.portString)) {
-			focus = selectedControl.port;
-			inputTimeout = 5;
-		}*/
+
+		if (GUI.Button (new Rect(396,360,200,20), xCoord))
+		{
+			focus = selectedControl.xbot;
+		}
+
+		if (GUI.Button (new Rect(396,380,200,20), yCoord))
+		{
+			focus = selectedControl.ybot;
+		}
+
+		if (GUI.Button (new Rect(396,400,200,20), k11))
+		{
+			focus = selectedControl.k11;
+		}
+
+		if (GUI.Button (new Rect(396,420,200,20), k12))
+		{
+			focus = selectedControl.k12;
+		}
+
+		if (GUI.Button (new Rect(396,440,200,20), k21))
+		{
+			focus = selectedControl.k21;
+		}
+
+		if (GUI.Button (new Rect(396,460,200,20), k22))
+		{
+			focus = selectedControl.k22;
+		}
+
+		if (GUI.Button (new Rect(696,360,200,20), xlight))
+		{
+			focus = selectedControl.xlight;
+		}
+
+		if (GUI.Button (new Rect(696,380,200,20), ylight))
+		{
+			focus = selectedControl.ylight;
+		}
+
+		if (GUI.Button (new Rect(696,400,200,20), intensity))
+		{
+			focus = selectedControl.intensity;
+		}
+
+		if (GUI.Button (new Rect(696,420,100,20), "Add Light"))
+		{
+			float xf, yf, itf;
+
+			if (float.TryParse (xlight, out xf) &&
+			    float.TryParse (ylight, out yf) &&
+			    float.TryParse (intensity, out itf))
+			{
+				SetupData.lightList.Add (new Light(SetupData.lightList.Count, xf, yf, itf));
+			}
+
+			xlight = string.Empty;
+			ylight = string.Empty;
+			intensity = string.Empty;
+		}
+
+		if (GUI.Button (new Rect(696,  460, 125,125), "Start Simulation\n without file"))
+		{
+			Application.LoadLevel ("Main");
+		}
 	}
 }
